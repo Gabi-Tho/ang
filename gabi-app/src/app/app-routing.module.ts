0d0647e8e8b85daef647cee4e4fc01b081f5eb59
Gabi-Tho <102246449+Gabi-Tho@users.noreply.github.com>
2023-02-15 09:15:14 -0500

    creating routes

--- gabi-app/src/app/app-routing.module.ts
+++ gabi-app/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { DetailComponent } from './detail/detail.component';
+import { ListeComponent } from './liste/liste.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "/", component:AccueilComponent},
+  {path: "/liste", component:ListeComponent},
+  {path: "/detail/:id", component:DetailComponent},
+  {path: "**", component:NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- gabi-app/src/app/app.module.ts
+++ gabi-app/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ProduitComponent } from './produit/produit.component';
+import { DetailComponent } from './detail/detail.component';
+import { EnteteComponent } from './entete/entete.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ListeComponent } from './liste/liste.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    ProduitComponent,
+    DetailComponent,
+    EnteteComponent,
+    NotFoundComponent,
+    ListeComponent
   ],
   imports: [
     BrowserModule,
